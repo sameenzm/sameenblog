54804991c596d18d40ffb1168ea5379453b914c0
Sameen <sameen916@qq.com>
2016-04-13 13:29:41 +0800

    _service/archive.js
    Optimized code

--- src/app/_service/archive.js
+++ src/app/_service/archive.js
@@ -5,59 +5,35 @@ define(function(require, exports, module){
     exports.getTopicList = function (cb) {
         var url = '/mock/topic.json';
         data.query(url, function (resp) {
-            if (resp.errCode === 200) {
-                cb(resp.data || []);
-            }
-            else {
-                cb([]);
-            }
-        }, 60000);
+            (resp.errCode === 200)?cb(resp.data || []):cb([]);
+        }, 5000);
     };
 
     exports.getArticle = function(cb){
         var url = '/mock/detail.json';
         data.query(url, function(resp){
-            if(resp.errCode === 200) {
-                cb(resp.data || []);
-            }
-            else {
-                cb([]);
-            }
+            (resp.errCode === 200)?cb(resp.data || []):cb([]);
         },5000);
     }
 
     exports.getNav = function(cb){
         var url = '/mock/topic.json';
         data.query(url, function(resp){
-            if(resp.errCode === 200) {
-                cb(resp.data || []);
-            }else {
-                cb([]);
-            }
+            (resp.errCode === 200)?cb(resp.data || []):cb([]);
         },5000);
     }
 
     exports.getArticleList = function (topicId, cb) {
         var url = '/mock/article-list-' + topicId + '.json';
         data.query(url, function (resp) {
-            if (resp.errCode === 200) {
-                cb(resp.data || []);
-            }
-            else {
-                cb([]);
-            }
+            (resp.errCode === 200)?cb(resp.data || []):cb([]);
         }, 5000);
     };
 
     exports.getArticleDetail = function (articleId, cb) {
         var url = '/mock/article-detail-' + articleId + '.json';
         data.query(url, function (resp) {
-            if (resp.errCode === 200) {
-                cb(resp.data || []);
-            }
-            else {
-                cb([]);
-            }
+            (resp.errCode === 200)?cb(resp.data || []):cb([]);
         }, 5000);
     };
 });
